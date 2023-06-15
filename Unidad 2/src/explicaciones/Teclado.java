a54011d86d00e0113bfdacc145d554195d88e028
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2023-06-15 20:37:37 -0300

    Declaramos y definimos un objeto Scanner.

---
+++ Unidad 2/src/explicaciones/Teclado.java
@@ -0,0 +1,11 @@
+package explicaciones;
+
+import java.util.Scanner;
+public class Teclado {
+    public static void main(String[] args) {
+
+        Scanner ent;                    // Declarar el objeto Scanner.
+        ent = new Scanner(System.in);   // Definir el objeto Scanner.
+
+    }
+}
