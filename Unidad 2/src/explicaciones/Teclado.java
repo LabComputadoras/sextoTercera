dcf8c90821bfaa515d8bb7ec42188563da32646f
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2023-06-15 20:43:33 -0300

    Imprimimos un número ingresado por el usuario.

--- Unidad 2/src/explicaciones/Teclado.java
+++ Unidad 2/src/explicaciones/Teclado.java
@@ -4,8 +4,11 @@ import java.util.Scanner;
 public class Teclado {
     public static void main(String[] args) {
 
-        Scanner ent;                    // Declarar el objeto Scanner.
-        ent = new Scanner(System.in);   // Definir el objeto Scanner.
+        int numero;
+        Scanner ent = new Scanner(System.in);  // Declaración y definición de un objeto Scanner.
 
+        System.out.println("Por favor, ingrese un número entero: ");
+        numero = ent.nextInt();                 // En la variable numero estamos guardando lo que se ingresa por teclado.
+        System.out.println("El número ingresado fue: " + numero);
     }
 }
