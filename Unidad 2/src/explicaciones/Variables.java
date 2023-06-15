ce85d6518ab1238da4e3e988ff81f0ae8c3f5c24
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2023-06-15 20:15:51 -0300

    Declaración de distintas variables.

---
+++ Unidad 2/src/explicaciones/Variables.java
@@ -0,0 +1,12 @@
+package explicaciones;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int numeroEntero;       // Es una variable para almacenar enteros.
+        double decimal;         // Variable para guardar números racionales.
+        String texto;           // Variable para guardar texto.
+        char caracter;          // Variable para guardar un caracter.
+        boolean bandera;        // Variable para guardar un estado lógico.
+    }
+}
