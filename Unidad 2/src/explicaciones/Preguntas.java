3ce13282eda6b6f552265567c363cf280ab17b78
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2023-07-06 18:55:41 -0300

    El usuario elige los valores de a y b.

--- Unidad 2/src/explicaciones/Preguntas.java
+++ Unidad 2/src/explicaciones/Preguntas.java
@@ -1,10 +1,18 @@
 package explicaciones;
 
+import java.util.Scanner;
+
 public class Preguntas {
     public static void main(String[] args) {
 
-        int a = 12;
-        int b = 11;
+        int a;
+        int b;
+        Scanner ent = new Scanner(System.in);
+
+        System.out.println("Por favor, ingrese el valor de A");
+        a = ent.nextInt();
+        System.out.println("Por favor, ingrese el valor de B");
+        b = ent.nextInt();
 
         if (b > a) {
             System.out.println("B es mayor que A");
