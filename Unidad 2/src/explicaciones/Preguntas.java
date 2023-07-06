0ba0f87ad21816f9161f6549192dbcfd7a30ea64
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2023-07-06 18:49:37 -0300

    Introducción a la estructura if.

---
+++ Unidad 2/src/explicaciones/Preguntas.java
@@ -0,0 +1,16 @@
+package explicaciones;
+
+public class Preguntas {
+    public static void main(String[] args) {
+
+        int a = 12;
+        int b = 11;
+
+        if (b > a) {
+            System.out.println("B es mayor que A");
+        }
+
+        System.out.println("Fin del programa");
+
+    }
+}
