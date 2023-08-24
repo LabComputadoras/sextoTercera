518d5d9799cd86bd6c2215361c9f771e19a39826
Dario Marin Roth <laboratoriodecomputadoraselectronicas64@epet14.edu.ar>
2023-08-24 20:23:17 -0300

    Definimos y creamos algunos vectores.

---
+++ Unidad 3/src/explicaciones/Vectores.java
@@ -0,0 +1,17 @@
+package explicaciones;
+
+public class Vectores {
+    public static void main(String[] args) {
+        int[] numeros;          // Declaración de un vector.
+        numeros = new int[5];
+        int[] otrosNumeros = {1, 1, 2, 3, 5, 8, 13};
+
+        numeros[0] = 1;
+        numeros[1] = 1;
+        numeros[2] = 2;
+        numeros[3] = 3;
+        numeros[4] = 5;
+        otrosNumeros[6] = 21;
+
+    }
+}
